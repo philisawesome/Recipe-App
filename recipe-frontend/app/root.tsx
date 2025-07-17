af6faa4cd9695b6b63f8a13e54103ae551cf0e78
alberttduong <albertduong0@gmail.com>
2025-07-17 00:47:22 -0700

    Set up react

--- recipe-frontend/app/root.tsx
+++ recipe-frontend/app/root.tsx
@@ -10,19 +10,6 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 
-export const links: Route.LinksFunction = () => [
-  { rel: "preconnect", href: "https://fonts.googleapis.com" },
-  {
-    rel: "preconnect",
-    href: "https://fonts.gstatic.com",
-    crossOrigin: "anonymous",
-  },
-  {
-    rel: "stylesheet",
-    href: "https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap",
-  },
-];
-
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
@@ -41,8 +28,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
   );
 }
 
+
 export default function App() {
-  return <Outlet />;
+	return (<div className="flex w-screen justify-center p-4">
+		<Outlet />
+	</div>);
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
--- recipe-frontend/app/routes/home.tsx
+++ recipe-frontend/app/routes/home.tsx
@@ -1,13 +1,11 @@
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Recipe Finder" },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+	return <div className="">Recipe Finder Home page</div>;
 }
