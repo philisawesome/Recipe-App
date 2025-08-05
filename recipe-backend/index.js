0cf293341b98f626855dd058b50b474926e49cba
Phil Huynh <phillhuynhh@gmail.com>
2025-08-04 17:48:41 -0700

    profile set up

--- recipe-backend/index.js
+++ recipe-backend/index.js
@@ -7,7 +7,10 @@ const authRoutes= require('./routes/auth');
 const app= express();
 
 //middleware stuff??? 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 
 
@@ -34,6 +37,7 @@ app.get('/', (req,res)=>{
 //auth
 app.use('/api/auth',authRoutes);
 
+//profile 
 app.get('/api/profile', (req,res)=>{
 
 
--- recipe-backend/models/Users.js
+++ recipe-backend/models/Users.js
@@ -4,19 +4,30 @@ const userSchema= new mongoose.Schema({
     username:{
         type:String,
         required:true,
-        unique:true
+        unique:true,
+        trim:true,
+        maxLength: 25
     },
     email:{
         type:String,
         required:true,
-        unique:true
+        unique:true,
+        trim:true
 
     },
     password:{
         type:String,
         required:true
 
+    },
+    name:{
+        type:String,
+        required:false,
+        trim:true,
+        maxLength: 25
+
     }
+
 
 
 });
